bdcf6311afdf6e4ee9e4802807ea03e1393739a9
Tahsin000 <tahsin.shabujglobal@gmail.com>
2026-01-06 15:01:51 +0600

    a # Please enter the commit message for your changes. Lines starting

---
+++ storage/framework/views/29fc0d0e547d1f216278c9c81643a6e5.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>SPA</title>
+    <?php echo app('Illuminate\Foundation\Vite')('resources/js/app.js'); ?>
+</head>
+<body>
+    <div id="app"></div>
+</body>
+</html>
+<?php /**PATH /Users/workstation/personal-project/spa-app/resources/views/welcome.blade.php ENDPATH**/ ?>
